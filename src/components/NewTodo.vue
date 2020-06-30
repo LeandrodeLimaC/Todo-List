80b5372627fdc349e8cbe70aefa9caf12bfabb08
leanlimac <leandrolima7472@gmail.com>
2020-06-30 14:13:20 -0300

    NewTodo w/ method addTodo working

---
+++ src/components/NewTodo.vue
@@ -0,0 +1,28 @@
+<template>
+  <form @submit.prevent="addTodo()" class="col-12 col-lf-6">
+      <input v-model="newTodo" placeholder="Enter a new Todo" type="text" class="form-control">
+  </form>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            newTodo: ""
+        }
+    },
+    methods: {
+        addTodo(){
+            if(this.newTodo.length > 1){
+                console.log(this.newTodo)
+                this.$emit("on-addTodo", this.newTodo)
+            }
+            this.newTodo = "";
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
