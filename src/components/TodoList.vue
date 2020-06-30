aa019d48ba6d2044d2a28b9da4f406d4687867f6
leanlimac <leandrolima7472@gmail.com>
2020-06-30 13:01:56 -0300

    Base interface/ Imported TodoComponent/ Data and Methods to our list

---
+++ src/components/TodoList.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+      <div class="row">
+          <div class="col-12">
+             <p class="display-3">
+                 Vue Crash Course
+             </p> 
+          </div>
+      </div>
+      <div class="row">
+          <p>Add create form</p>
+      </div>
+      <div class="row">
+          <div class="col-12 col-lg-6">
+              <ul class="list-group">
+                  <Todo v-for="(todo, index) in todos" :key="index"
+                  :todoString="todo.todoString"
+                  :completed="todo.completed"
+                  />
+              </ul>
+          </div>  
+      </div>
+  </div>
+</template>
+
+<script>
+import Todo from './Todo';
+
+export default {
+    components:{
+        Todo
+    },
+    data(){
+        return{
+            todos: [
+                {todoString: "Make Angular Course", completed : false},
+                {todoString: "Cook some food", completed : true},
+                {todoString: "Make Youtube videos", completed : true},
+                {todoString: "Publish these videos", completed : false},
+            ]
+        }
+    },
+    methods: {
+        addTodo(newTodo){
+            this.todo.push({
+                todoString: newTodo,
+                completed: false
+            });
+        },
+        toggleTodo(todo){
+            todo.completed = !todo.completed;
+        },
+        editTodo(todo, newTodoString){
+            todo.todoString = newTodoString;
+        },
+        deleteTodo(deleteTodo){
+            this.todos = this.todos.filter(
+                todo => todo !== deleteTodo);
+        },
+    },
+
+}
+</script>
+
+<style>
+
+</style>
